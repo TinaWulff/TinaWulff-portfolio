45d93778f60378aa58116e2b64fb74a34d525ac2
Tina Wulff <tina@tinawulff.dk>
2026-02-12 14:33:22 +0100

    opdateret introtekst

--- src/components/index.intro.js
+++ src/components/index.intro.js
@@ -1,6 +1,6 @@
 
 import "./index-intro.scss";
-import CV from '../assets/TinaWulff-CV.pdf';
+import CV from '../assets/TinaWulff-CV2026.pdf';
 
 
 
@@ -9,17 +9,14 @@ export default function introDiv() {
     introDivElm.className = "intro-div"
 
     introDivElm.innerHTML = `
-    <p>I study front-end web development at Roskilde Technical Institute,
-        where I will complete my final exam in the spring of 2026.</p>
-        
-    <p>I have a background in fine art from The Royal Danish Academy of Arts
-        (graduated in 2020), along with additional education in UX/UI and graphic design.</p>
+   <p>I am a frontend developer specialized in React and NEXT.js. I will graduate from the Frontend Web Development program at Roskilde Technical School in March 2026.</p>
+   <p> I am currently seeking my next challenge and looking for a position in programming where I can contribute with my skills while continuing to learn and deepen my expertise.</p>
 
-    <p>This portfolio showcases my skills and ongoing projects,
-        which will continuously be expanding as i evolve my knowlegde.</p>
+    <p>I have a background in Fine Art from the Royal Danish Academy of Fine Arts (graduated in 2020), complemented by additional education in UX/UI and graphic design.</p>
 
+    <p>This portfolio showcases my skills and ongoing projects and will continue to expand as I grow professionally. Please note that my current knowledge extends beyond what is presented here, as my focus is on intensive study and development, and the site may not always reflect my most recently acquired skills.</p>
     <p class="read-cv blue">..Read more about my experience in my CV</p>
-    <a href="${CV}" download="TinaWulff-CV.pdf">Download CV (Danish) as PDF here </a><i class="fa-solid fa-angle-right"></i>
+    <a href="${CV}" download="TinaWulff-CV2026.pdf">Download CV (updated 2026) as PDF here </a><i class="fa-solid fa-angle-right"></i>
     `
 
     return introDivElm
@@ -28,3 +25,5 @@ export default function introDiv() {
 /*
 <p class="read-more">...Read more <a href="#">about me </a><i class="fa-solid fa-angle-right"></i></p>
 */
+
+
